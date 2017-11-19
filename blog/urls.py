1dff38abab0272518b1288139170634239c62ec5
4eshir <shadowrun777@ukr.net>
2017-11-19 15:01:23 +0200

    T

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
-from . import views
+from django.contrib import admin
 
 urlpatterns = [
-    url(r'^$', views.post_list, name='post_list'),
+    url(r'^admin/', admin.site.urls),
 ]
